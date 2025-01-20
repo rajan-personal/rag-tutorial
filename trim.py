094e0802c610d58900e19d2e4165a51dc5953126
ugly-saifu <gamermdmd37@gmail.com>
2025-01-20 13:25:13 +0530

    optimzed

--- trim.py
+++ trim.py
@@ -1,44 +1,40 @@
-import sys
+from PIL import Image
+import numpy as np
+from collections import deque
 
-sys.setrecursionlimit(100000)
+def flood_fill(img, start_pos, color):
+    width, height = img.size
+    img_array = np.array(img)
+    white = np.array([255, 255, 255])
+    
+    queue = deque([start_pos])
+    done = set()
+    
+    while queue:
+        x, y = queue.popleft()
+        if (x, y) in done:
+            continue
+            
+        img_array[y, x] = color
+        done.add((x, y))
+        
+        for nx, ny in [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]:
+            if (0 <= nx < width and 
+                0 <= ny < height and 
+                (nx, ny) not in done and 
+                np.array_equal(img_array[ny, nx], white)):
+                queue.append((nx, ny))
+    
+    return Image.fromarray(img_array)
 
-with open('console_output.txt', 'w') as f:
-    sys.stdout = f
+# Main processing
+img = Image.open("new_sample.png")
+if img.mode != 'RGB':
+    img = img.convert('RGB')
 
-    from PIL import Image
+white = (255, 255, 255)
+red = (255, 0, 0)
 
-    img = Image.open("new_sample.png")
-    if img.mode != 'RGB':
-        img = img.convert('RGB')
-
-    def mark_pixel(x1, y1, color):
-        if f'{x1}_{y1}' in done:
-            return
-
-        img.putpixel((x1, y1), color)
-        done.append(f'{x1}_{y1}')
-
-        try:
-            if x1 > 0 and f'{x1 - 1}_{y1}' not in done and img.getpixel((x1 - 1, y1)) in [white, color]:
-                mark_pixel(x1 - 1, y1, color)
-
-            if y1 > 0 and f'{x1}_{y1 - 1}' not in done and img.getpixel((x1, y1 - 1)) in [white, color]:
-                mark_pixel(x1, y1 - 1, color)
-
-            if x1 < img.width - 1 and f'{x1 + 1}_{y1}' not in done and img.getpixel((x1 + 1, y1)) in [white, color]:
-                mark_pixel(x1 + 1, y1, color)
-
-            if y1 < img.height - 1 and f'{x1}_{y1 + 1}' not in done and img.getpixel((x1, y1 + 1)) in [white, color]:
-                mark_pixel(x1, y1 + 1, color)
-        except RecursionError:
-            print(f'Recursion limit reached at pixel: ({x1}, {y1})')
-
-    x = 0
-    y = 0
-    done = []
-    white = (255, 255, 255)
-    red = (255, 0, 0)
-    mark_pixel(x, y, red)
-    img.save('new_sample2.png')
-
-sys.stdout = sys.__stdout__
+# Process starting from top-left corner
+img = flood_fill(img, (0, 0), red)
+img.save('new_sample2.png')
